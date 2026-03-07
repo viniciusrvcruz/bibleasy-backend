742ce68502bb26d8af405474ec306b12f0803508
Vinicius Cruz <viniciusrobertopro@gmail.com>
2026-03-07 17:35:32 -0300

    refactor: comment out CloudflareRealIp middleware in app.php for future consideration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->prepend(CloudflareRealIp::class);
+        // $middleware->prepend(CloudflareRealIp::class);
         $middleware->trustProxies(at: '*');
 
         $middleware->alias([
